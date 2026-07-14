a2ddd723690344d94124d759192040e46cd14b2d
Dương  Hưng <duonghung5637@gmail.com>
2026-07-14 11:09:00 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print('--- HỆ THỐNG TIẾP NHẬN BỆNH NHÂN --- ');
+name_patient = input('nhap tên bệnh nhân: ');
+age = int(input('Mời bạn nhập tuổi: '));
+symptom = input('Mời bạn nhập triệu chứng bệnh: ');
+
+print('-- PHIẾU KHÁM BỆNH ---');
+print('Tên bệnh nhân: ',name_patient);
+print('Tuổi: ',age);
+print('Triệu chứng: ',symptom);
+ádasdasd
